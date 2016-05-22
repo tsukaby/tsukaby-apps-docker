70892af59099a63cf96a2772538024451db6ca50
Shuya Tsukamoto <shuya.tsukamoto@gmail.com>
2016-05-22 22:59:22 +0900

    New version label as sha1.

--- package_for_elastic_beanstalk.sh
+++ package_for_elastic_beanstalk.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-now=`date +"%Y%m%d_%H%M%S"`
-file_name=${now}_tsukaby-apps.zip
+app_sha1=$1
+
+file_name=tsukaby-apps-${app_sha1}.zip
 
 # Create zip
 cd eb
@@ -14,5 +15,5 @@ aws s3 mv ${file_name} s3://tsukaby-apps/binary/
 # Create eb version
 aws elasticbeanstalk create-application-version \
   --application-name tsukaby-apps \
-  --version-label ${now} \
+  --version-label ${app_sha1} \
   --source-bundle S3Bucket="tsukaby-apps",S3Key=binary/${file_name}
